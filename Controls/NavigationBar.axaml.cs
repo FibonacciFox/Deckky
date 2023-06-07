f9d8f3ad044650f7b3cd833c896a50b5ba27b2dc
FibonacciFox <shamanwolfrus@gmail.com>
2023-06-07 17:20:11 +0700

    Initial commit

---
+++ Controls/NavigationBar.axaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Deckky.Controls;
+
+public partial class NavigationBar : UserControl
+{
+    public NavigationBar()
+    {
+        InitializeComponent();
+        
+    }
+
+    private void InitializeComponent()
+    {
+        AvaloniaXamlLoader.Load(this);
+    }
+}
