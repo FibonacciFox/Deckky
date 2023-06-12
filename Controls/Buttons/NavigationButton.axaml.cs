43735075ffaec795602869fd5870dcad2730bc43
FibonacciFox <shamanwolfrus@gmail.com>
2023-06-12 17:54:04 +0700

    Added Buttons

---
+++ Controls/Buttons/NavigationButton.axaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Deckky.Controls.Buttons;
+
+public partial class NavigationButton : UserControl
+{
+
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<NavigationButton, string>(nameof(Title));
+
+    public string Title
+    {
+        get => ButtonTitle.Text;
+        set => ButtonTitle.Text = value;
+    }
+    
+    public NavigationButton()
+    {
+        InitializeComponent();
+    }
+
+}
--- Controls/NavigationBar.axaml.cs
+++ Controls/NavigationBar.axaml.cs
@@ -1,5 +1,4 @@
-﻿using Avalonia;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
 namespace Deckky.Controls;
@@ -9,11 +8,5 @@ public partial class NavigationBar : UserControl
     public NavigationBar()
     {
         InitializeComponent();
-        
-    }
-
-    private void InitializeComponent()
-    {
-        AvaloniaXamlLoader.Load(this);
     }
 }
