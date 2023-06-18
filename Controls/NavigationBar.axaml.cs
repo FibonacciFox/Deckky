703823123d8d27d6d9136b194eea8004f87b6336
FibonacciFox <shamanwolfrus@gmail.com>
2023-06-18 18:28:01 +0700

    Переработано меню, добавлены стили.

--- Controls/NavigationBar.axaml.cs
+++ Controls/NavigationBar.axaml.cs
@@ -1,47 +1,18 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-using Avalonia.Controls;
+﻿using Avalonia.Controls;
 using Avalonia.Input;
-using Avalonia.Markup.Xaml;
-using Deckky.Controls.Buttons;
 
 namespace Deckky.Controls;
 
 public partial class NavigationBar : UserControl
 {
-    protected int _currentButtonSelected = 0;
-
-    private List<NavigationButton> _navigationButtonList = new();
     
     public NavigationBar()
     {
         InitializeComponent();
-        
-        _navigationButtonList.Add(NavButton1);
-        _navigationButtonList.Add(NavButton2);
-        _navigationButtonList.Add(NavButton3);
-        _navigationButtonList.Add(NavButton4);
-        _navigationButtonList.Add(NavButton5);
-        
-        NavButton1.Classes.Add("Selected");
-        
-        this.KeyUp += OnKeyUp;
     }
 
     private void OnKeyUp(object? sender, KeyEventArgs e)
     {
-        if (e.Key == Key.LeftCtrl)
-        {
-            _navigationButtonList[_currentButtonSelected].Classes.Remove("Selected");
-            _currentButtonSelected -= 1;
-            _navigationButtonList[_currentButtonSelected].Classes.Add("Selected");
-        }
         
-        if (e.Key == Key.RightCtrl)
-        {
-            _navigationButtonList[_currentButtonSelected].Classes.Remove("Selected");
-            _currentButtonSelected += 1;
-            _navigationButtonList[_currentButtonSelected].Classes.Add("Selected");
-        }
     }
 }
---
+++ Controls/NavigationItem.axaml.cs
@@ -0,0 +1,55 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+
+namespace Deckky.Controls;
+
+public partial class NavigationItem : UserControl
+{
+    public static readonly StyledProperty<string> TitleProperty =
+        AvaloniaProperty.Register<NavigationItem, string>(nameof(Title));
+    
+    /// <summary>
+    /// Счетчик чего либо на кнопке
+    /// </summary>
+    public static readonly StyledProperty<int> CounterProperty =
+        AvaloniaProperty.Register<NavigationItem, int>(nameof(Counter));
+    
+    /// <summary>
+    /// Отображать счетчик
+    /// </summary>
+    public static readonly StyledProperty<bool> IsVisibleCounterProperty =
+        AvaloniaProperty.Register<NavigationItem, bool>(nameof(IsVisibleCounter));
+    
+    /// <summary>
+    /// Заголовок NavigationItem
+    /// </summary>
+    public string Title
+    {
+        get => ItemTitle.Text;
+        set => ItemTitle.Text = value;
+    }
+    
+    /// <summary>
+    /// Счетчик чего либо на кнопке
+    /// </summary>
+    public int Counter
+    {
+        get => int.Parse(ItemCounter.Text);
+        set => ItemCounter.Text = value.ToString();
+    }
+    
+    /// <summary>
+    /// Отображать счетчик
+    /// </summary>
+    public bool IsVisibleCounter
+    {
+        get => ItemCounter.IsVisible;
+        set => ItemCounter.IsVisible = value;
+    }
+    
+    public NavigationItem()
+    {
+        InitializeComponent();
+    }
+    
+}
