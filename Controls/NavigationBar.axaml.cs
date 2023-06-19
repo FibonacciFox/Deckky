90b6284a70c193c496c6321f57ba92125413f04b
FibonacciFox <shamanwolfrus@gmail.com>
2023-06-19 17:16:22 +0700

    Добавлено переключение вкладок меню бара

--- Controls/NavigationBar.axaml.cs
+++ Controls/NavigationBar.axaml.cs
@@ -1,18 +1,53 @@
-﻿using Avalonia.Controls;
-using Avalonia.Input;
+﻿using System.Collections.Generic;
+using Avalonia;
+using Avalonia.Controls;
 
 namespace Deckky.Controls;
 
 public partial class NavigationBar : UserControl
 {
-    
+
+    /// <summary>
+    /// Заголовок NavigationItem
+    /// </summary>
+    public static readonly StyledProperty<int> ActiveItemProperty =
+        AvaloniaProperty.Register<NavigationItem, int>(nameof(ActiveItem));
+
+    public int ActiveItem
+    {
+        get
+        {
+            return GetValue(ActiveItemProperty);
+        }
+        set
+        {
+            var lastActiveItem = GetValue(ActiveItemProperty);
+            _navigationItems[lastActiveItem].Selected = false;
+            SetValue(ActiveItemProperty, value);
+            _navigationItems[value].Selected = true;
+        }
+    }
+
     public NavigationBar()
     {
         InitializeComponent();
-    }
 
-    private void OnKeyUp(object? sender, KeyEventArgs e)
-    {
+        foreach (var Item in NavigationItems.Children)
+        {
+            _navigationItems.Add((NavigationItem)Item);
+        }
         
     }
+
+    public void PreviousItem()
+    {
+        ActiveItem -= 1;
+    }
+    
+    public void NextItem()
+    {
+        ActiveItem += 1;
+    }
+    
+    private List<NavigationItem> _navigationItems = new();
 }
--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Avalonia.Input;
 
 namespace Deckky;
 
@@ -7,6 +8,20 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        
+        KeyDown+= OnKeyDown;
     }
-    
+
+    private void OnKeyDown(object? sender, KeyEventArgs e)
+    {
+        if (e.Key == Key.LeftCtrl)
+        {
+            NavBar.PreviousItem();
+        }
+        
+        if (e.Key == Key.RightCtrl)
+        {
+            NavBar.NextItem();
+        }
+    }
 }
